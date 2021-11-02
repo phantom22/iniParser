f3103dbedd648a0eaa9710b46b2d3e6789031125
Maksym Naumenko <maksymnaumenko2002@mail.ru>
2021-11-02 17:51:34 +0100

    Add files via upload

--- Basics/Basics.cpp
+++ Basics/Basics.cpp
@@ -5,15 +5,13 @@
 #include "ini.cpp"
 using namespace std;
 
-int main() {
+int WinMain() {
 
     string path = "input/test.ini";
-
     //cout << "Enter file to read: ";
     //cin >> path; 
     ini::file x = ini::file(path);
-    string test = "[abc]\nx=1\n\n[a]y=3";
-    //cout << test.substr(0+1, 4-1) << endl;
+    cout << x.getInt("cat1", "a") << endl;
     return 0;
 
 }
--- Basics/ini.cpp
+++ Basics/ini.cpp
@@ -4,23 +4,67 @@
 #include <vector>
 #include <tuple>
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 namespace ini {
+
+	//enum ExpectedTypes {
+	//	tInt
+	//};
+
 	class file {
 
 		private:
-			string contents;
+			vector<tuple<string, string>> categories;
+
+			string getStringValue(string category, string property) {
+				string res = "";
+				int catI = -1;
+				for (size_t i = 0; i < categories.size(); i++) {
+					string cat = std::get<0>(categories[i]);
+					if (cat == category) {
+						catI = i;
+						break;
+					}
+				}
+				if (catI != -1) {
+					string &props = std::get<1>(categories[catI]);
+					stringstream ss (props);
+					string line;
+					char n = '\n';
+					// read line by line
+					while (getline(ss, line, n)) {
+						const char delimiter = '=';
+						size_t i = line.find(delimiter);
+						if (i != string::npos) {
+							string pname = line.substr(0, i);
+							if (pname == property) {
+								string pval = line.substr(i + 1);
+								res = pval;
+								break;
+							}
+						}
+						else 
+							continue;
+					}
+					if (res != "") {
+						return res;
+					}
+					else
+						throw invalid_argument("Property [" + property + "] not found in [" + category + "]!");
+				}
+				else {
+					throw invalid_argument("Category [" + category + "] not found!");
+				}
+
+			}
 
 		public:
-			vector<tuple<string, string>> categories; 
-			/*[
-			 *  ["cat", "blob"]
-			 *]
-			 * get<0>(get<1>(properties[0]))
-			 */
 
 			file(string path) {
+
+				string contents;
 
 				ifstream file (path);
 				if (file.is_open()) {
@@ -30,42 +74,57 @@ namespace ini {
 					file.close();
 				}
 
-				bool isPlitted = false;
-				int beforeNextCat = 0;
+				bool isSplitted = false;
+				size_t position = 0;
 				// split into categories
-				while (!isPlitted) {
+				while (!isSplitted) {
 
-					string cut = contents.substr(beforeNextCat);
+					string cut = contents.substr(position);
 					size_t bracket1 = cut.find("[");
 					size_t bracket2 = cut.find("]");
-					const int tmp = beforeNextCat;
 					string category = cut.substr(bracket1 + 1, bracket2 - bracket1 - 1);
+	
+					size_t blobStart = position + bracket2 + 2;
+					size_t blobEnd = cut.substr(bracket2 + 1).find("[");
 
-					beforeNextCat = cut.substr(bracket2 + 1).find("[");
-					if (beforeNextCat == string::npos) {
-						beforeNextCat = contents.length() - (tmp + bracket2 + 1);
-						isPlitted = true;
+					if (blobEnd == string::npos) {
+						blobEnd = contents.length() - blobStart + 1;
+						isSplitted = true;
+					}
+					else {
+						blobEnd += (position + bracket2) - blobStart;
+						position = blobStart + blobEnd;
 					}
 
-					string blob = contents.substr(tmp + bracket2 + 2, beforeNextCat);
-
+					string blob = contents.substr(blobStart, blobEnd);
 					categories.push_back(make_tuple(category, blob));
-					
+
 				}
 
 			}
 
-			//template<typename T>
-			void get(string category/*, string property*/) {
-				cout << "dio porco" << endl;
-				//auto cat = find(categories.begin(), categories.end(), category);
-				//if (cat != categories.end()) {
-				//	//size_t i = cat - categories.begin();
-				//	//cout << std::get<0>(categories[i]) << endl;
-				//}
-				//else {
-				//	cout << "Not found!" << endl;
-				//}
+			int getInt(string cat, string prop) {
+				string val = getStringValue(cat, prop);
+				char* i = &val[0];
+				return atoi(i);
+			}
+
+			float getFloat(string cat, string prop) {
+				string val = getStringValue(cat, prop);
+				char *s = &val[0];
+				char* e;
+				return strtof(s, &e);
+			}
+
+			double getDouble(string cat, string prop) {
+				string val = getStringValue(cat, prop);
+				char* i = &val[0];
+				return atof(i);
+			}
+
+			bool getBool(const char cat[], const char prop[]) {
+				string val = getStringValue(cat, prop);
+				return val == "1" || val == "true" ? true : false;
 			}
 		
 	};
